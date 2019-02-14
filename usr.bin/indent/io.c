299910f3fb03cb7ab1beab7eeb1574c3851b844a
sh4nnu <manikishanghantasala@gmail.com>
2019-02-14 16:59:40 +0530

    indent(1): have the memset invocation somewhat more canonical.
    While correct, the previous invocation was somewhat more error prone.

--- usr.bin/indent/io.c
+++ usr.bin/indent/io.c
@@ -634,7 +634,7 @@ parsefont(struct fstate *f, const char *s0)
 {
 	const char *s = s0;
 	int     sizedelta = 0;
-	memset(f, 0, sizeof *f);
+	memset(f, '\0', sizeof(*f);
 	while (*s) {
 		if (isdigit((unsigned char)*s))
 			f->size = f->size * 10 + *s - '0';
