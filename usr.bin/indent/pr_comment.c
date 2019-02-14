258430dd0231dea0287c41afe94ec79165f77c1a
sh4nnu <manikishanghantasala@gmail.com>
2019-02-14 20:47:27 +0530

    indent(1): don't produce unnecessary blank lines.
    Don't force a blank line between an opening brace and a block comment -- not
    even if -bbb (blank lines before block comments) is on.

--- usr.bin/indent/pr_comment.c
+++ usr.bin/indent/pr_comment.c
@@ -80,6 +80,7 @@ __RCSID("$NetBSD: pr_comment.c,v 1.10 2016/02/25 13:23:27 ginsbach Exp $");
 #include <string.h>
 #include <ctype.h>
 #include "indent_globs.h"
+#include "indent_codes.h"
 
 /*
  * NAME:
@@ -216,7 +217,7 @@ pr_comment(void)
 		char       *t = e_com;
 		e_com = s_com + 2;
 		*e_com = 0;
-		if (blanklines_before_blockcomments)
+		if (blanklines_before_blockcomments && ps.last_token != lbrace)
 		    prefix_blankline_requested = 1;
 		dump_line();
 		e_com = s_com = t;
