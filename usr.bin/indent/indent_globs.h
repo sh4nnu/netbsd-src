5d580fb9d85b40760c42abc430eef525b2232f47
sh4nnu <manikishanghantasala@gmail.com>
2019-02-15 17:39:41 +0530

    indent(1): improve handling of boxed comments indentation
    The trick is to copy everything from the start of the line into the buffer
    that stores newlines and comments until indent finds a brace or an else.
    pr_comment() will use that information to calculate the original indentation
    of the boxed comment.
    
    This requires storing two pieces of information: the real start of the
    buffer (sc_buf) and the start of the comment (save_com).

--- usr.bin/indent/indent_globs.h
+++ usr.bin/indent/indent_globs.h
@@ -147,8 +147,9 @@ EXTERN char   *buf_ptr;			/* ptr to next character to be taken from
 					 * in_buffer */
 EXTERN char   *buf_end;			/* ptr to first after last char in in_buffer */
 
-EXTERN char    save_com[sc_size];	/* input text is saved here when looking for
+EXTERN char    sc_buf[sc_size];	/* input text is saved here when looking for
 				 	 * the brace after an if, while, etc */
+EXTERN char    save_com;		/* start of the comment stored in sc_buf */
 EXTERN char   *sc_end;			/* pointer into save_com buffer */
 
 EXTERN char   *bp_save;			/* saved value of buf_ptr when taking input
@@ -250,7 +251,13 @@ EXTERN struct parser_state {
 	int     box_com;	/* set to true when we are in a "boxed"
 				 * comment. In that case, the first non-blank
 				 * char should be lined up with the comment / */
-	int     comment_delta, n_comment_delta;
+	int     comment_delta, /* used to set up indentation for all lines
+						    * of a boxed comment after the first one */
+			
+			n_comment_delta; /* remembers how many columns there were
+							  * before the start of a box comment so that
+							  * forthcoming lines of the comment are
+							  * indented properly */
 	int     cast_mask;	/* indicates which close parens potentially
 				 * close off casts */
 	int     not_cast_mask;	/* indicates which close parens definitely
